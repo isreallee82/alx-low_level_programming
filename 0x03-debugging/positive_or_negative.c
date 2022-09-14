4430a4701eea8e65c79644923569f9ff47b1f65e
isreallee82 <isreallee82@gmail.com>
2022-09-14 16:04:29 -0700

    lee

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -10,12 +10,11 @@
 int main(void)
 {
 	int n;
-
+	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	if (n > 0)
-	{
+	;{
 		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
@@ -26,6 +25,5 @@ int main(void)
 	{
 		printf("%d is negative\n", n);
 	}
-
 	return (0);
 }
